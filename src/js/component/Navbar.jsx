f786bdd63fbdf75d8a705689cfff9210475511db
INVIiICTUS <j.enriquealorenzo@gmail.com>
2023-03-31 09:24:29 +0000

    navbar y fondo

--- src/js/component/Navbar.jsx
+++ src/js/component/Navbar.jsx
@@ -7,13 +7,13 @@ function NavbarCustom() {
         <div className="collapse navbar-collapse" id="navbarNav">
           <ul className="navbar-nav mx-auto">
             <li className="nav-item active">
-              <a className="nav-link text-black" href="">
+              <a className="nav-link text-white p-auto" href="">
                 CONTACTA <span className="sr-only">(current)</span>
               </a>
             </li>
 
             <li className="nav-item">
-              <a className="nav-link text-black" href="">
+              <a className="nav-link text-white p-auto" href="">
                 PROYECTOS
               </a>
             </li>
