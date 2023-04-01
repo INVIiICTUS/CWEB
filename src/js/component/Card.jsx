20e2c61fb2c9c9a3af3f35e624dd293b3b0bc0f7
INVIiICTUS <j.enriquealorenzo@gmail.com>
2023-04-01 18:47:09 +0000

    principio de cartas

--- src/js/component/Card.jsx
+++ src/js/component/Card.jsx
@@ -1,8 +1,66 @@
 import React from "react";
-function Card() {
-	return (
-		<div><h1>hola</h1></div>
-			);
+import Button from "react-bootstrap/Button";
+import Card from "react-bootstrap/Card";
+
+function Galery() {
+  return (
+    <>
+      <div className="container d-flex">
+        <div className="p-3 m-3">
+          <Card style={{ width: "18rem" }}>
+            <Card.Img variant="top" src="" />
+            <Card.Body>
+              <Card.Title>Card Title</Card.Title>
+              <Card.Text>
+                Some quick example text to build on the card title and make up
+                the bulk of the card's content.
+              </Card.Text>
+              <Button variant="primary">Go somewhere</Button>
+            </Card.Body>
+          </Card>
+        </div>
+		<div className="p-3 m-3">
+          <Card style={{ width: "18rem" }}>
+            <Card.Img variant="top" src="" />
+            <Card.Body>
+              <Card.Title>Card Title</Card.Title>
+              <Card.Text>
+                Some quick example text to build on the card title and make up
+                the bulk of the card's content.
+              </Card.Text>
+              <Button variant="primary">Go somewhere</Button>
+            </Card.Body>
+          </Card>
+        </div>
+		<div className="p-3 m-3">
+          <Card style={{ width: "18rem" }}>
+            <Card.Img variant="top" src="" />
+            <Card.Body>
+              <Card.Title>Card Title</Card.Title>
+              <Card.Text>
+                Some quick example text to build on the card title and make up
+                the bulk of the card's content.
+              </Card.Text>
+              <Button variant="primary">Go somewhere</Button>
+            </Card.Body>
+          </Card>
+        </div>
+		<div className="p-3 m-3">
+          <Card style={{ width: "18rem" }}>
+            <Card.Img variant="top" src="" />
+            <Card.Body>
+              <Card.Title>Card Title</Card.Title>
+              <Card.Text>
+                Some quick example text to build on the card title and make up
+                the bulk of the card's content.
+              </Card.Text>
+              <Button variant="primary">Go somewhere</Button>
+            </Card.Body>
+          </Card>
+        </div>c
+      </div>
+    </>
+  );
 }
 
-export default Card;
+export default Galery;
