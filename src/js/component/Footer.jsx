81ae51baff43a38df23fea7e862bebad87b16d56
INVIiICTUS <j.enriquealorenzo@gmail.com>
2023-04-04 08:50:17 +0000

    footer casi casi

--- src/js/component/Footer.jsx
+++ src/js/component/Footer.jsx
@@ -1,10 +1,18 @@
 import React from "react";
 
 function Footer() {
-	return (
-		<div>
-			
-		</div>
-	);
+  return (
+    <div>
+      <footer
+        className="footer  bg-dark container-fluid d-flex justify-content-center align-items-center fixed-bottom"
+        /* style="height: 150px" */
+        style={{ height: "50px" }}
+      >
+        <p className="text-white fw-bold">
+          Copyright © INVIiICTUS November 2022
+        </p>
+      </footer>
+    </div>
+  );
 }
 export default Footer;
