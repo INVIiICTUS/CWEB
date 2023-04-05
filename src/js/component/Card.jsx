92a1b7de792925df221e020e706c7a54cd2ca04a
INVIiICTUS <j.enriquealorenzo@gmail.com>
2023-04-05 09:05:20 +0000

    seguimos con errores responsive// conflictos entre css y bootstrap

--- src/js/component/Card.jsx
+++ src/js/component/Card.jsx
@@ -4,25 +4,25 @@ import Corazonvendup from "../../../public/Corazonvendup.png";
 function Galery() {
   return (
     <div className="row aling-items-center justify-content-center m-2 p-2">
-      <section className="center-aligns">
-        <img src="https://i.blogs.es/ef0d01/harry-potter/840_560.jpeg" alt="" />
+      <div className="secction col-lg-4 col-md-6 col-12 my-1 mb-5 ">
+        <img src="https://i.blogs.es/ef0d01/harry-potter/840_560.jpeg" alt="Proyecto Harry Potter" />
 
         <img
           src="https://imgs.hipertextual.com/wp-content/uploads/2013/10/breaking-bad-final.jpg"
-          alt=""
+          alt="Proyecto Breaking Bad"
         />
-        <img src={Corazonvendup} role="img" aria-label="Vendup" />
+        <img src={Corazonvendup} alt="Proyecto Final Vendup" />
 
         <img
           src="https://i0.wp.com/www.lacasadeel.net/wp-content/uploads/2023/03/Star-Wars-Recast-Star-Wars-Midjourney-8-2.jpg"
-          alt=""
+          alt="Proyecto Stars Wars"
         />
 
         <img
           src="https://www.casinozaragoza.es/wp-content/uploads/2018/09/poker.jpg"
-          alt=""
+          alt="Proyecto Generador de Cartas random"
         />
-      </section>
+      </div>
     </div>
   );
 }
